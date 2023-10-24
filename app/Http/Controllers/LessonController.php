1a7506b1194d98026e4928e78119ecabc5a90a5c
Andres <andres.porfirio.tablet@gmail.com>
2023-10-24 16:04:19 -0300

    base code

---
+++ app/Http/Controllers/LessonController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LessonController extends Controller
+{
+    public function example(){
+        return view('example');
+    }
+}
---
+++ resources/views/example.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Google Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
+
+        <!-- CSS Reset -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
+
+        <!-- Milligram CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+
+        <title>Laravel</title>
+    </head>
+    <body class="antialiased">
+        <h1>Blade Example</h1>
+
+        <h2>Test</h2>
+    </body>
+</html>
