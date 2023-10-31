b9c667ae0e13053af08a6fa8c1f5b6512b981a36
Andres <andres.porfirio.tablet@gmail.com>
2023-10-31 17:07:10 -0300

    added lesson code

--- resources/views/example.blade.php
+++ resources/views/example.blade.php
@@ -1,15 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    
-    @include("shared.head")
 
-    <body class="antialiased">
-        <h1>Cars</h1>
+@include('shared.head')
+
+<body class="antialiased">
+
+    <div class="container px-6 py-16 mx-auto text-center dark:bg-black">
+        <div class="max-w-lg mx-auto">
+            <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Cars</h1>
+            <p class="mt-6 text-gray-500 dark:text-gray-300">
+                Check my Cars List!
+            </p>
+        </div>
 
         @forelse ($cars as $c)
-            <x-car :model="$c->model" :brand="$c->brand" :hp="$c->hp" :photoUrl="$c->photoUrl"/>
+            <x-car :model="$c->model" :brand="$c->brand" :hp="$c->hp" :photoUrl="$c->photoUrl" />
         @empty
             <p>No Car available!</p>
         @endforelse
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/shared/head.blade.php
+++ resources/views/shared/head.blade.php
@@ -8,8 +8,7 @@
     <!-- CSS Reset -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css">
 
-    <!-- Milligram CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css">
+    @vite('resources/css/app.css')
 
     <title>Laravel Lesson</title>
 </head>
